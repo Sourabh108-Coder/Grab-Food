7835f386b5d454fd59feef83d8679053c2483a35
Sourabh108-Coder <kumarsourabh7164@gmail.com>
2026-09-06 19:49:16 +0530

    Updated Sidebar code

--- admin/src/Components/Sidebar.jsx
+++ admin/src/Components/Sidebar.jsx
@@ -1,33 +1,36 @@
-import React from 'react'
+import React from 'react';
 import { IoIosAddCircleOutline } from "react-icons/io";
 import { RiListCheck3 } from "react-icons/ri";
 import { GiStorkDelivery } from "react-icons/gi";
+import { MdDashboard } from "react-icons/md";
 import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
-    <div className='side'>
+    <aside className="side">
 
       <NavLink to="/" className="same">
-       <div>🏠</div>
-       <p>Dashboard</p>
-      </NavLink>
-      <NavLink to="/add" className='same'>
-        <div><IoIosAddCircleOutline className='logo' /></div>
-        <p>Add-Items</p>
+        <MdDashboard className="side-icon" />
+        <span>Dashboard</span>
       </NavLink>
 
-      <NavLink to="/list" className='same'>
-        <div><RiListCheck3 className='logo'/></div>
-        <p>List-Items</p>
+      <NavLink to="/add" className="same">
+        <IoIosAddCircleOutline className="side-icon" />
+        <span>Add Items</span>
       </NavLink>
 
-      <NavLink to="/order" className='same'>
-        <div><GiStorkDelivery className='logo'/></div>
-        <p>Order-Item</p>
+      <NavLink to="/list" className="same">
+        <RiListCheck3 className="side-icon" />
+        <span>List Items</span>
       </NavLink>
-    </div>
-  )
-}
 
-export default Sidebar
+      <NavLink to="/order" className="same">
+        <GiStorkDelivery className="side-icon" />
+        <span>Orders</span>
+      </NavLink>
+
+    </aside>
+  );
+};
+
+export default Sidebar;
