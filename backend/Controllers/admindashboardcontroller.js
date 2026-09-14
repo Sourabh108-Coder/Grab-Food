10cfecb93ce8999a2d1109bffee21bfd3cab9cda
Sourabh108-Coder <kumarsourabh7164@gmail.com>
2026-09-14 21:09:06 +0530

    Fixing return issue

--- backend/Controllers/admindashboardcontroller.js
+++ backend/Controllers/admindashboardcontroller.js
@@ -141,7 +141,7 @@ exports.dashboard = async (req, res) => {
         // RESPONSE
         // =========================
 
-        res.status(200).json({
+        return res.status(200).json({
 
             success: true,
 
@@ -187,7 +187,7 @@ exports.dashboard = async (req, res) => {
 
         console.log("Dashboard Error:", error);
 
-        res.status(500).json({
+        return res.status(500).json({
 
             success: false,
 
